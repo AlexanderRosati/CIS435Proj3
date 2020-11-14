0be357a5d52279820bb27b0446b85fe67b8d5a4a
nate1gb <necarey13@gmail.com>
2020-11-14 16:13:30 -0500

    Working on 6-4
    Mostly functional. Just need to add error checking and final request display

--- 6-4/customer_login.php
+++ 6-4/customer_login.php
@@ -1,41 +1,37 @@
 <?php
     
     include('../view/header.php');
-
+    
     $error = "";
 
     // submit email entered
     if(isset($_POST['submit'])){
-        $custEmail = 'email';
-        echo $custEmail;
+        if(!empty($_POST['customerEmail'])){
+            include('../model/database.php');
+            $custEmail = htmlentities($_POST['customerEmail']);
+    
+            $query = 'SELECT firstName, lastName FROM customers WHERE email=:email';
+    
+            $prep_stmt = $db->prepare($query);
+            $prep_stmt->execute(['email' => $custEmail]);
+            $row = $prep_stmt->fetchAll();           
+            $row_count = $prep_stmt->rowCount();
+    
+            if($row_count == 1){                
+                foreach($row as $loginCustomer){
+                    $custName = $loginCustomer['firstName'].' '.$loginCustomer['lastName'];
+                }
+                $_SESSION['customerName'] = $custName;
+                header('Location: register_product.php');
+            }
+            else{
+                $error = "A customer with that email address was not found.";
+            } 
 
-        $query = 'SELECT * FROM customer WHERE email = :email';
-
-        $prep_stmt = $db->prepare($query);
-        $prep_stmt->execute(['email' => $custEmail]);
-
-        $customerResult = $prep_stmt->fetchAll();
-
-        echo $customerResult->name . '<br>';
-
-        /* if($customerResult){
-            header('Location: register_product.php');
         }
-        else{
-            $error = "A customer with that email address was not found.";
-        } */
-
     }
 
-
-
-
-
-
-
-
 ?>
-
 
 <main>
 <h2>Customer Login</h2>
@@ -45,10 +41,7 @@
     <input type="text" name="customerEmail" id="customerEmail" placeholder="Email">
     <input name="submit" type="submit" value="Login"> <br><br>
 </form>
-
-
+<p class="error"><?php echo $error; ?></p>
 </main>
-
-
 
 <?php include('../view/footer.php'); ?>
--- 6-4/register_product.php
+++ 6-4/register_product.php
@@ -1,21 +1,35 @@
 <?php
     
     include('../view/header.php');
-
-
-
-
-
-
-
-
+    // set customer variable from previous page
+    $currentCustomer = $_SESSION['customerName'];
+    include('../model/database.php');
 
 ?>
+<main>
+<h2>Register Product</h2>
+<form method="POST" action="<?php $_SERVER['PHP_SELF']?>">
+    <label class="fixed-width" for="customerName">Customer:</label><?php echo $currentCustomer;?><br><br>
+    <label class="fixed-width" for="Product">Product:</label>
+    <select id="procudtList" name="productList">
+    <option value="">---Products---</option>
+        <?php
+            $productQuery = 'SELECT name FROM products';
+            $prep_stmt = $db->prepare($productQuery);
+            $prep_stmt->execute();
+            $productList = $prep_stmt->fetchAll(PDO::FETCH_OBJ); 
+        ?>
+        <?php 
+        foreach($productList as $productItem): ?>
+                <option value="<?php echo $productItem->name; ?>"><?php echo $productItem->name; ?></option>
+	    <?php endforeach; ?>
+	</select><br><br>
+    <label class="fixed-width"></label>
+    <input name="submit" type="submit" value="Register Product"></input> <br><br>
+</form>
 
 
-
-
-
+</main>
 
 
 
